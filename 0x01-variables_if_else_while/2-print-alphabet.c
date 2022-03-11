8b8249655504f0b42a7f838909e94a81676adcb4
Qllela <sylvillela8@gmail.com>
2022-03-11 03:24:47 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print-alphabet.c
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -4,43 +4,54 @@
 
 #include <stdio.h>
 
+
+
 /**
- * main - print the last digit of the number stored in the variable n
- * Return: 0 if program executed successfully
-*/
+ *
+ *  * main - print the last digit of the number stored in the variable n
+ *
+ *   * Return: 0 if program executed successfully
+ *
+ *    */
 
 int main(void)
 
 {
 
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d ", n, n % 10);
-if (n % 10 > 5)
+		int n;
 
-{
 
-printf("and is greater than 5\n");
 
-}
+			srand(time(0));
 
-else if (n % 10 == 0)
+				n = rand() - RAND_MAX / 2;
 
-{
+					printf("Last digit of %d is %d ", n, n % 10);
 
-printf("and is 0\n");
+						if (n % 10 > 5)
 
-}
+								{
 
-else if (n % 10 < 6 && n % 10 != 0)
+											printf("and is greater than 5\n");
 
-{
+												}
 
-printf("and is less than 6 and not 0\n");
+							else if (n % 10 == 0)
 
-}
+									{
 
-return (0);
+												printf("and is 0\n");
+
+													}
+
+								else if (n % 10 < 6 && n % 10 != 0)
+
+										{
+
+													printf("and is less than 6 and not 0\n");
+
+														}
+
+									return (0);
 
 }
