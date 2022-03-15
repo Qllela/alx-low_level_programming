5ddd3aed00fd8b74b47a94ef012ed7e5ff5ec3ba
Qllela <sylvillela8@gmail.com>
2022-03-15 14:15:29 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,8 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Brennan D Baraban
+*/
+
 #include "main.h"
 
 /**
@@ -9,10 +14,11 @@
 int print_last_digit(int n)
 
 {
-int last_digit = n % 10;
-if (last_digit < 0)
-last_digit *= -1;
-putchar(last_digit + '0');
-return (last_digit);
+	int last_digit = n % 10;
+	if (last_digit < 0)
+	
+		last_digit *= -1;
+	putchar(last_digit + '0');
 
+	return (last_digit);
 }
