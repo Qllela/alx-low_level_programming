4fa56b21241a6f4f416d76262e7bc8d361792b12
Qllela <sylvillela8@gmail.com>
2022-03-15 13:30:35 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,19 @@
 int print_sign(int n)
 
 {
-if (n > 0)
-{
-putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-putchar(48);
-return (0);
-}
-else if (n < 0)
-{
-putchar('-');
-}
-return (-1);
+	if (n > 0)
+	{	
+		putchar('+');
+		return (1);
+	}	
+	else if (n == 0)
+	{			
+		putchar(48);
+		return (0);
+	}	
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}	
 }
