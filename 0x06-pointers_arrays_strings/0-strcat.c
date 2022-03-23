7fafe74089619485c64c61949cb5278f46bc3b06
Qllela <sylvillela8@gmail.com>
2022-03-23 09:52:55 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,17 @@
  * Return: A pointer to the destination string @dest.
 */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-dest_len++;
-
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+do
+{
+a++;
+dest[i] = src[a];
+i++;
+}
+while (src[a] != '\0');
 return (dest);
 }
